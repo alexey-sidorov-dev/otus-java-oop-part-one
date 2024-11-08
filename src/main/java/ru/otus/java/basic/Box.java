fef563f01a7c264dff51bd9f90d02aa69a3da1dc
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2024-11-08 13:55:50 +0300

    fix class Box after code review

--- src/main/java/ru/otus/java/basic/Box.java
+++ src/main/java/ru/otus/java/basic/Box.java
@@ -12,7 +12,7 @@ public class Box {
         this.color = color;
         this.size = Math.abs(size);
         this.isOpened = false;
-        printBoxInfo();
+        printInfo();
     }
 
     public String getColor() {
@@ -46,7 +46,7 @@ public class Box {
         return item;
     }
 
-    public void printBoxInfo() {
+    public void printInfo() {
         Joiner joiner = Joiner.on("; ");
         System.out.println(joiner.join("Информация о коробке\n" +
                         "Размер: " + size,
@@ -58,34 +58,27 @@ public class Box {
     public void putItem(String item) {
         if (!isOpened) {
             System.out.println("Коробка закрыта, невозможно положить " + item);
-        } else {
-            if (this.item != null) {
-                System.out.println(
-                        "Коробка заполнена, невозможно положить " + item
-                );
-            } else {
-                System.out.println(
-                        "В коробку положили " + item
-                );
-                this.item = item;
-            }
+            return;
         }
+        if (this.item != null) {
+            System.out.println("Коробка заполнена, невозможно положить " + item);
+            return;
+        }
+
+        System.out.println("В коробку положили " + item);
+        this.item = item;
     }
 
     public void dropItem() {
         if (!isOpened) {
             System.out.println("Коробка закрыта, невозможно выкинуть " + this.item);
-        } else {
-            if (this.item == null) {
-                System.out.println(
-                        "Коробка пустая, невозможно выкинуть предмет"
-                );
-            } else {
-                System.out.println(
-                        "Из коробки выкинули " + this.item
-                );
-                this.item = null;
-            }
+            return;
         }
+        if (this.item == null) {
+            System.out.println("Коробка пустая, невозможно выкинуть предмет");
+        }
+
+        System.out.println("Из коробки выкинули " + this.item);
+        this.item = null;
     }
 }
